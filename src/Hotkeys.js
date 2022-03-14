314efd905e7f9507a88adc6400151fe63cb63fd2
Vladimir Talan <talan.vladimir@gmail.com>
2022-03-14 10:20:21 +0200

    fork: add Hotkeys class

---
+++ src/Hotkeys.js
@@ -0,0 +1,67 @@
+class SetHotkeys {
+    constructor(hotkeys) {
+      this.hotkeys = hotkeys;
+      this.buffer = [];
+    }
+  
+    keyFromKeyboardEvent(event) {
+      let key = null;
+      if (event.key) key = event.key;
+      else {
+        const identifier = event.keyIdentifier;
+        if (identifier.search(/^U\+/) === -1) key = identifier;
+        else
+          key = String.fromCharCode(parseInt(identifier.replace(/^U\+/, ''), 16));
+      }
+      return key.toLowerCase();
+    }
+  
+    isTyping(active = document.activeElement) {
+      return active.tagName == 'TEXTAREA' || active.tagName == 'INPUT';
+    }
+  
+    isMetaKeys() {
+      return (
+        this.buffer.includes('control') ||
+        this.buffer.includes('alt') ||
+        this.buffer.includes('shift')
+      );
+    }
+  
+    start() {
+      window.addEventListener(
+        'keydown',
+        event => {
+          const key = this.keyFromKeyboardEvent(event);
+          if (!this.buffer.includes(key)) {
+            this.buffer.push(key);
+            if (
+              this.hotkeys.has(key) === true &&
+              this.isTyping() === false &&
+              this.isMetaKeys() === false
+            ) {
+              this.hotkeys.get(key)();
+            }
+          }
+        },
+        true,
+      );
+  
+      window.addEventListener(
+        'keyup',
+        event => {
+          const key = this.keyFromKeyboardEvent(event);
+  
+          if (this.buffer.includes(key))
+            this.buffer = this.buffer.filter(elem => {
+              return elem !== key;
+            });
+        },
+        true,
+      );
+  
+      window.addEventListener('focus', event => {
+        this.buffer = [];
+      });
+    }
+  }
