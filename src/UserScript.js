ee68d934c548a70bd7ff7dcac409a99a3228c08b
Vladimir Talan <talan.vladimir@gmail.com>
2022-05-07 22:37:57 +0300

    fork: add userscript v0.1

---
+++ src/UserScript.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         _Test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @updateURL    https://raw.githubusercontent.com/TalanVladimir/TampermonkeyCore/master/src/UserScript.js
+// @downloadURL  https://raw.githubusercontent.com/TalanVladimir/TampermonkeyCore/master/src/UserScript.js
+
+// @match       *://*/*
+// @match       *
+
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=githubusercontent.com
+// @grant        none
+
+// @require     https://raw.githubusercontent.com/TalanVladimir/TampermonkeyCore/master/src/HotkeysClass.js
+
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  console.log('TEST UPDATE OF THIS SCRIPT 0.1');
+  console.log('TEST UPDATE OF THIS SCRIPT 0.1');
+  console.log('TEST UPDATE OF THIS SCRIPT 0.1');
+
+  // Your code here...
+})();
